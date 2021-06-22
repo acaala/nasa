94779f556e6906d3a8fdfa4927b05f7f66cad838
Liam Watts <elnewatts@gmail.com>
2021-06-22 08:02:25 +0000

    Updated git ignore, refactored code

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,1 +1,11 @@
+<svelte:head>
+    <style>
+        body {
+          background: rgb(32, 32, 35);
+          font-family: 'Poppins';
+        }
+      </style>
+  <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+</svelte:head>
+
 <slot></slot>
